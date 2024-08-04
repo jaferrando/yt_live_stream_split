96805dfc99f64a0cae72a0d29363b0048d176303
Juan Alvarez Ferrando <37533515+jaferrando@users.noreply.github.com>
2024-08-04 14:36:33 +0200

    User _mix files as valid input and use the CSV without the suffix

--- split_video.ps1
+++ split_video.ps1
@@ -4,15 +4,23 @@ param (
 
 $mp4_file = Get-Item $file
 $name = $mp4_file.Basename
-
-$mp3_name = "{0}.mp3" -f $name
-$chapters_name = "{0}.csv" -f $name
+if ($name -like "*_mix") {
+  $orig_name = $name -replace "_mix"
+  Write-Host $orig_name
+  $mp3_name = "{0}.mp3" -f $orig_name
+  Write_Host $mp3_name
+  $chapters_name = "{0}.csv" -f $orig_name
+  Write-Host $chapters_name
+} else {
+  $mp3_name = "{0}.mp3" -f $name
+  $chapters_name = "{0}.csv" -f $name
+}
 
 $chapters = Import-Csv -Path $chapters_name | Select-Object -Skip 1 | Foreach-Object {
   $start = $_.start
   $end = $_.end
   $song = ("{0}.mp4" -f $_.song)
-  Write-Host "ffmpeg -y -i $output_name -avoid_negative_ts make_zero -fflags +genpts -ss $start -to $end -c:a aac $song"
+  Write-Host "ffmpeg -y -i $file -avoid_negative_ts make_zero -fflags +genpts -ss $start -to $end -c:a aac $song"
 }
 
 
